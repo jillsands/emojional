a01b871ebbb0f386c2516c85c13e6dbfddcd6910
Jillian Sands <jesands21@gmail.com>
2021-05-03 19:05:51 -0400

    Complete Iteration 4

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     let emojis = ["🧘🏼‍♀️": "yoga", "🏋🏾‍♀️": "weight lifting"]
+    let customMessages = ["yoga":["in through the nose, out through the mouth", "smile", "inhale deep"], "weight lifting": ["never lose your strength", "remember who you are", "stay strong"]]
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = (sender.titleLabel?.text)!
-        let alertController = UIAlertController(title: "Alert!", message: emojis[selectedEmotion], preferredStyle: UIAlertController.Style.alert)
+        let options = customMessages[emojis[selectedEmotion]!]!
+        let emojiMessage = options.randomElement()
+        let alertController = UIAlertController(title: "Alert!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
