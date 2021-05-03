d43104ee035ca0119db97607dc0d2f0c0edc4f59
Jillian Sands <jesands21@gmail.com>
2021-05-03 17:46:17 -0400

    Initial Commit

---
+++ emojional/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emojional
+//
+//  Created by Jillian Sands on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
